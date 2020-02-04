b14bba3c3bbf6a1e3dd557ad3cf4bad4ec535f80
marie-marthe <marie.telbois@gmail.com>
2020-02-03 22:18:15 -0400

    Ajouts de symfony pour demarer le projet
    Signed-off-by: marie-marthe <marie.telbois@gmail.com>

--- src/ML/CoreBundle/DependencyInjection/Configuration.php
+++ src/ML/CoreBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ML\CoreBundle\DependencyInjection;
+namespace ML\TicketingBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ml_core');
+        $rootNode = $treeBuilder->root('ml_ticketing');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
