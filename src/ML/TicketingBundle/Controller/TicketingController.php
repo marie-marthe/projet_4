1b1dc81ff0ea0650bcafad872d4f854620943dad
marie-marthe <marie.telbois@gmail.com>
2020-02-05 21:09:19 -0400

    add schedule informations with the css file
    Signed-off-by: marie-marthe <marie.telbois@gmail.com>

---
+++ src/ML/TicketingBundle/Controller/TicketingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ML\TicketingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TicketingController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('MLTicketingBundle:Ticketing:index.html.twig');
+    }
+
+    public function scheduleAction()
+    {
+        return $this->render('MLTicketingBundle:Ticketing:schedule.html.twig');
+    }
+}
