e4d7cb7dd607b04b16f5e174f4dab65c28efb983
marie-marthe <marie.telbois@gmail.com>
2020-02-06 22:46:30 -0400

    add and replace informations
    Signed-off-by: marie-marthe <marie.telbois@gmail.com>

--- src/ML/TicketingBundle/Controller/TicketingController.php
+++ src/ML/TicketingBundle/Controller/TicketingController.php
@@ -2,7 +2,10 @@
 
 namespace ML\TicketingBundle\Controller;
 
+use ML\TicketingBundle\Entity\Bill;
+use ML\TicketingBundle\Form\BillType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class TicketingController extends Controller
 {
@@ -14,5 +17,48 @@ class TicketingController extends Controller
     public function scheduleAction()
     {
         return $this->render('MLTicketingBundle:Ticketing:schedule.html.twig');
+    }
+
+    public function rateAction()
+    {
+        return $this->render('MLTicketingBundle:Ticketing:rate.html.twig');
+    }
+
+    public function bookingAction(Request $request)
+    {
+        $bill = new Bill();
+        $form = $this->createForm(BillType::class, $bill);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            // Find quantity of tickets for a day and add the number of tickets for this order
+            $quantityByDay = $em->getRepository('MLTicketingBundle:Bill')->countTicketsByDay($bill->getVisitDay());
+            $totalWithOrder = $quantityByDay + count($bill->getTickets());
+
+            // Check if maximum accommodation capacity is reached
+            if ($totalWithOrder > $this->getParameter('maxCapacity')) {
+                $request->getSession()->getFlashBag()->add('full', 'Complet pour le '.$bill->getVisitDay()->format('d-m-Y'));
+
+            } else {
+                // Find and set the correct price for each ticket
+                $this->get('ml_ticketing.compute_price')->givePrice($bill);
+
+                // Check if something wrong happened during payment process
+                if ($this->get('ml_ticketing.stripe_service')->validCharge($bill) === false) {
+                    $request->getSession()->getFlashBag()->add('noValidCharge', 'Une erreur lors du paiement est survenue!');
+
+                } else {
+                    $em->persist($bill);
+                    $em->flush();
+                    $request->getSession()->getFlashBag()->add('success', 'Votre commande a bien été prise en compte.');
+                }
+            }
+            return $this->redirectToRoute('ml_ticketing_booking');
+        }
+
+        return $this->render('MLTicketingBundle:Ticketing:booking.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
--- src/ML/TicketingBundle/CoreBundle/MLCoreBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ML\CoreBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MLCoreBundle extends Bundle
-{
-}
